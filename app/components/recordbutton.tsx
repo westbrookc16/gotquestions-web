2ae05588a7d3a386c2779169310cb92f46534987
Chris Westbrook <westbchris@gmail.com>
2025-03-19 21:11:19 -0400

    Added chad cn button component.

--- app/components/recordbutton.tsx
+++ app/components/recordbutton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button } from "@/components/ui/button"
 import { useRecordVoice } from "../hooks/userecord";
 import {useState} from "react";
 import Content from "@/app/components/content";
@@ -7,7 +8,7 @@ const RecordButton = () => {
   const { startRecording, stopRecording, recording, text } = useRecordVoice();
   const [isLoading,setIsLoading]=useState(false);
   return (<div>
-    <button disabled={isLoading} onClick={recording ? stopRecording : startRecording}>{recording ? "Stop" : "Start"}</button>
+    <Button disabled={isLoading} onClick={recording ? stopRecording : startRecording}>{recording ? "Stop" : "Start"}</Button>
     <br />{text}
     <Content text={text} setLoading={setIsLoading} />
   </div>);
